ad95a68f8b4222821f4a33f05c64f49a14c9b390
wiedld <dlw405@gmail.com>
2015-07-06 23:19:18 -0700

    why strings are immutable - bus error 10 may be the reason

---
+++ pointers_3.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+void strcpy_2 (char *dest, char *src){
+    do {
+        *dest = *src;
+        dest++;
+        src++;
+    } while (*src != '\0');
+}
+
+
+void strcat_2(char *dest, char *src){
+    // get to end of 1st str (to concat onto end)
+    while (*dest != '\0'){
+        dest++;
+    }
+    // copy src to the end of dest, and
+    // change value of where dest points to
+    do {
+        *dest = *src;
+        dest++;
+        src++;
+    } while (*src != '\0');
+}
+
+
+void strcat_3(char *dest, char *src, char* output){
+    // copy 1st half of string
+    do {
+        *output = *dest;
+        dest++;
+        output++;
+    } while (*dest != '\0');
+    // copy 2nd half of string
+    do {
+        *output = *src;
+        output++;
+        src++;
+    } while (*src != '\0');
+}
+
+
+main(){
+    char *sam = "Sam";
+    char sam_2[4] = {};
+    strcpy_2(sam_2, sam);
+    printf ("%s should be equal to %s \n", sam, sam_2);
+
+    // this give "Bus error: 10" which usually means a mis-aligned memory
+    char *mc = "Mc";
+    char *end = "Donalds";
+    // strcat_2(mc, end);
+    // printf ("%s is disturbing \n", mc);
+
+    // try it this way, treating the string ointer as going to
+    // an immutable space in memory. So create a new memeory space
+    // for concat product
+    char result[10] = {};
+    strcat_3(mc, end, result);
+    printf ("%s is disturbing \n", result);
+
+}
