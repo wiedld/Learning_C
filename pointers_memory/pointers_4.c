1a75f3247905616deb21d9b3752e9ca50dc582f1
wiedld <dlw405@gmail.com>
2015-07-16 14:19:08 -0700

    updated pointer casting

--- pointers_memory/pointers_4.c
+++ pointers_memory/pointers_4.c
@@ -4,7 +4,7 @@
 
 // Generalized function, using pointers & pointer math
 
-void *reverse_1 (void *base, int n, int elemSize, void(*swapFn) (void *, void *))
+void reverse_1 (void *base, int n, int elemSize, void (*swapFn) (void *, void *))
 {
     for (int i = 0; i < (n/2); i++){
         // find pointer to each element in array
@@ -20,22 +20,28 @@ void *reverse_1 (void *base, int n, int elemSize, void(*swapFn) (void *, void *)
 
 
 // swap function for integers
-void swapInt(int *ap, int *bp){
-    int temp = *ap;
-    *ap = *bp;
-    *bp = temp;
+void swapInt(void *ap, void *bp){
+    int *_ap = (int *) ap;
+    int *_bp = (int *) bp;
+
+    int temp = *_ap;
+    *_ap = *_bp;
+    *_bp = temp;
 }
 
 
 // swap function for strings
-void swapChar(char *ap, char *bp){
-    char temp = *ap;
-    *ap = *bp;
-    *bp = temp;
+void swapChar(void *ap, void *bp){
+    char *_ap = (char *) ap;
+    char *_bp = (char *) bp;
+
+    char temp = *_ap;
+    *_ap = *_bp;
+    *_bp = temp;
 }
 
 
-// Testing
+// Testing.
 int main()
 {
     //  TEST with array of int
@@ -49,8 +55,10 @@ int main()
     printf("\n");
 
     // TEST with string (array of char)
-    char goat[] = {'m','o','u','n','t','a','i','n',' ','g','o','a','t'};
-    int str_size = 13;
+        // two identical ways to make string
+    char goat[] = "mountain goat";
+    // char goat[] = {'m','o','u','n','t','a','i','n',' ','g','o','a','t','\0'};
+    int str_size = 14;
 
     reverse_1(goat, str_size, sizeof(char), swapChar);
     for (int idx = 0; idx < str_size; idx++){
@@ -58,5 +66,6 @@ int main()
     }
     printf("\n");
 
+    return 0;
 }
 
