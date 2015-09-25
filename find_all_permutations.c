a22724a654ffe82bd66ad94f60a761ebac8954bb
wiedld <dlw405@gmail.com>
2015-09-24 21:54:00 -0700

    small cleanup

--- find_all_permutations.c
+++ find_all_permutations.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-/* Function to swap values at two pointers */
+
 void swap(int *x, int *y)
 {
     int temp;
@@ -11,54 +10,49 @@ void swap(int *x, int *y)
     *y = temp;
 }
 
-/* Function to print permutations of string
-   This function takes three parameters:
-   1. String
-   2. Starting index of the string
-   3. Ending index of the string. */
-void permute(int *a, int l, int r, int **results, int *counter)
+
+void permute(int *a, int l, int n, int **results, int *counter)
 {
-  // printf("%d\n", counter[0]);
   int eaRes = counter[0];
   int i;
-   if (l == r){
-      printf("position in array: %d\n", eaRes);
-      results[eaRes] = malloc(sizeof(int) * 4);
-      printf("ADD");
-      for (int id=0; id < 4; id++){
+   if (l == (n-1)){
+      // alloc memory for num array, to add
+      results[eaRes] = malloc(sizeof(int) * n);
+      // add values
+      for (int id=0; id < n; id++){
         results[eaRes][id] = a[id];
-        printf("%d", results[eaRes][id]);
-        // printf("added %d\n", *results[eaRes]);
       }
-      printf("\n");
+      // indexing result array, with a counter being updated by
+      // all descendent recursive calls
       counter[0] = counter[0] + 1;
    } else {
-       for (i = l; i <= r; i++)
+       for (i = l; i < n; i++)
        {
           swap((a+l), (a+i));
-          permute(a, l+1, r, results, counter);
+          permute(a, l+1, n, results, counter);
           swap((a+l), (a+i)); //backtrack
        }
    }
 }
 
-/* Driver program to test above functions */
+
 int main()
 {
-    int str[] = {1,2,3,4};
-    int **outcome = malloc(sizeof(int *) * 24);
-
+    int array[] = {1,2,3,4};
     int n = 4;
+    int numPermu = 24;
+    int **outcome = malloc(sizeof(int *) * numPermu);
     int counter[] = {0};
-    permute(str, 0, n-1, outcome, counter);
 
-    for (int i=0; i < 24; i++){
+    permute(array, 0, n, outcome, counter);
+
+    for (int i=0; i < numPermu; i++){
       printf("permutation: ");
-      for (int j=0; j < 4; j++){
+      for (int j=0; j < n; j++){
         printf("%d", outcome[i][j]);
-        // free(*startOutcome[i]);
       }
       printf("\n");
+      free(outcome[i]);
     }
     return 0;
 }
